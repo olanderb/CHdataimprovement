4a408c20435ca731a7c2226bb66dd9fc3c83b949
olanderb <billyolander@gmail.com>
2020-01-27 16:36:10 +0000

    Changed syntax to match Codebook variable name suggestions

--- MLI_201909_ENSAN_CH.R
+++ MLI_201909_ENSAN_CH.R
@@ -27,13 +27,13 @@ codebook <- a %>%
 # Select and rename the key variables and values used for the analalysis --------
 
 #standardize names of key variables
-data <- data  %>% select(adm1_name = q11a_nom_region, #first adminstrative division
-                                            adm2_name = q12_nom_cercle, #second administrative division
+data <- data  %>% select(ADMIN1Name = q11a_nom_region, #first adminstrative division
+                                            ADMIN2Name = q12_nom_cercle, #second administrative division
                                             HDDS = SDAM_ENSAN022018, #Household Dietary Diversity Score
-                                            FCG = FCClass, #Food Consumption Groups from the Food Consumption Score
+                                            FCSCat = FCClass, #Food Consumption Groups from the Food Consumption Score
                                             HHS = HHSscore, #Household Hunger Score
                                             rCSI = CSI_reduit, #reduced coping strategies
-                                            LHCS = max_coping_strat, #livelihood coping strategies
+                                            LhHCSCat = max_coping_strat, #livelihood coping strategies
                                             Weight = Weigth_new1, #survey weights - if none - delete this line
                                             choc_subi = q101a_chocs_subis_derniers6_mois #one example of contributing factor
                                             )
@@ -47,11 +47,11 @@ data <- data %>%  mutate(FCG = case_when(
   FCG == "Limite" ~ "Borderline",       
   FCG == "Acceptable" ~ "Acceptable"
 ),
-LHCS = case_when(
-  LHCS == "Pas de stratégies" ~ "NoStrategies", # Put accents to stratigies so that the code works properly
-  LHCS == "Stratégies de stress" ~ "StressStrategies",
-  LHCS == "Stratégies de crise" ~ "CrisisStrategies",
-  LHCS == "Stratégies d'urgence" ~ "EmergencyStrategies"
+LhHCSCat = case_when(
+  LhHCSCat == "Pas de stratégies" ~ "NoStrategies", # Put accents to stratigies so that the code works properly
+  LhHCSCat == "Stratégies de stress" ~ "StressStrategies",
+  LhHCSCat == "Stratégies de crise" ~ "CrisisStrategies",
+  LhHCSCat == "Stratégies d'urgence" ~ "EmergencyStrategies"
 ),
 CH_HDDS = case_when(
   HDDS >= 5 ~ "Phase1", 
@@ -82,7 +82,7 @@ CH_HHS =
 ###create tables of the proportion by administrative areas and then apply the CH indicator specific and 20% rule to each indicator
 #Household Dietarty Diversity Score
 HDDStable <- data %>% 
-  group_by(adm1_name, adm2_name) %>%
+  group_by(ADMIN1Name, ADMIN2Name) %>%
   count(CH_HDDS,wt=Weight) %>%
   drop_na() %>%
   mutate(n = 100 * n / sum(n)) %>%
@@ -99,11 +99,11 @@ CH_HDDStable <- HDDStable %>% mutate(indicator = "HDDS",
                                        `Phase3` >= 20 | phase345 >= 20 ~ 3, #if 20% or more is in phase 3 or the sum of phase3, 4 and 5 is more than 20% then assign phase 3
                                        `Phase2` >= 20 | phase2345 >= 20 ~ 2, #if 20% or more is in phase 2 or the sum of phase 2, 3, 4 and 5 is more than 20% then assign phase 2
                                        TRUE ~ 1)) %>% #otherwise assign phase 1
-                                       select(indicator, adm1_name, adm2_name, HDDS_Phase1 = Phase1, HDDS_Phase2 = Phase2, HDDS_Phase3 = Phase3, HDDS_Phase4 = Phase4, HDDS_Phase5 = Phase5, HDDS_finalphase = finalphase) #select only relevant variables, rename them with indicator name and order in proper sequence
+                                       select(indicator, ADMIN1Name, ADMIN2Name, HDDS_Phase1 = Phase1, HDDS_Phase2 = Phase2, HDDS_Phase3 = Phase3, HDDS_Phase4 = Phase4, HDDS_Phase5 = Phase5, HDDS_finalphase = finalphase) #select only relevant variables, rename them with indicator name and order in proper sequence
 
 #Food Consumption Groups
 FCGtable <- data %>% 
-  group_by(adm1_name, adm2_name) %>%
+  group_by(ADMIN1Name, ADMIN2Name) %>%
   count(FCG,wt=Weight) %>%
   drop_na() %>%
   mutate(n = 100 * n / sum(n)) %>%
@@ -116,11 +116,11 @@ CH_FCGtable <-  FCGtable %>%  mutate(indicator = "FCG", PoorBorderline = Poor + 
   between(Poor,5,10) ~ 2, #if % of people are between 5 and 10%  then phase2
   between(Poor,10,20) & PoorBorderline < 30 ~ 2, #if % of people in poor food group are between 20 and 20% and the % of people who are in poor and borderline is less than 30 % then phase2
   between(Poor,10,20) & PoorBorderline >= 30 ~ 3)) %>% #if % of people in poor food group are between 20 and 20% and the % of people who are in poor and borderline is less than 30 % then phase2
-  select(indicator, adm1_name, adm2_name, FCG_Poor = Poor, FCG_Borderline = Borderline, FCG_Acceptable = Acceptable, FCG_finalphase = finalphase) #select only relevant variables and order in proper sequence
+  select(indicator, ADMIN1Name, ADMIN2Name, FCG_Poor = Poor, FCG_Borderline = Borderline, FCG_Acceptable = Acceptable, FCG_finalphase = finalphase) #select only relevant variables and order in proper sequence
 
 #Household Hunger Score
 HHStable <- data %>% 
-  group_by(adm1_name, adm2_name) %>%
+  group_by(ADMIN1Name, ADMIN2Name) %>%
   count(CH_HHS,wt=Weight) %>%
   drop_na() %>%
   mutate(n = 100 * n / sum(n)) %>%
@@ -136,11 +136,11 @@ CH_HHStable <- HHStable %>% mutate(indicator = "HHS", phase2345 = `Phase2` + `Ph
                                      Phase3 >= 20 | phase345 >= 20 ~ 3,
                                      Phase2 >= 20 | phase2345 >= 20 ~ 2,
                                      TRUE ~ 1)) %>% 
-                                    select(indicator, adm1_name, adm2_name, HHS_Phase1 = Phase1, HHS_Phase2 = Phase2, HHS_Phase3 = Phase3, HHS_Phase4 = Phase4, HHS_Phase5 = Phase5, HHS_finalphase = finalphase)
+                                    select(indicator, ADMIN1Name, ADMIN2Name, HHS_Phase1 = Phase1, HHS_Phase2 = Phase2, HHS_Phase3 = Phase3, HHS_Phase4 = Phase4, HHS_Phase5 = Phase5, HHS_finalphase = finalphase)
 
 #reduced consumption score
 rCSItable <- data %>% 
-  group_by(adm1_name, adm2_name) %>%
+  group_by(ADMIN1Name, ADMIN2Name) %>%
   count(CH_rCSI,wt=Weight) %>%
   drop_na() %>%
   mutate(n = 100 * n / sum(n)) %>%
@@ -154,26 +154,26 @@ CH_rCSItable <- rCSItable %>% mutate(indicator = "rCSI",
                                          Phase3 >= 20 ~ 3, 
                                          Phase2 >= 20 | rcsi23 >= 20 ~ 2,
                                          TRUE ~ 1
-                                       )) %>% select(indicator, adm1_name, adm2_name, rCSI_Phase1 = Phase1, rCSI_Phase2 = Phase2, rCSI_Phase3 =Phase3, rCSI_finalphase = finalphase)
+                                       )) %>% select(indicator, ADMIN1Name, ADMIN2Name, rCSI_Phase1 = Phase1, rCSI_Phase2 = Phase2, rCSI_Phase3 =Phase3, rCSI_finalphase = finalphase)
 
 
 #Livelihood Coping Strategies
-LHCStable <- data %>% 
-  group_by(adm1_name, adm2_name) %>%
-  count(LHCS,wt=Weight) %>%
+LhHCSCattable <- data %>% 
+  group_by(ADMIN1Name, ADMIN2Name) %>%
+  count(LhHCSCat,wt=Weight) %>%
   drop_na() %>%
   mutate(n = 100 * n / sum(n)) %>%
   ungroup() %>%
-  spread(key = LHCS, value = n) %>% replace(., is.na(.), 0) %>% mutate_if(is.numeric, round, 1)
+  spread(key = LhHCSCat, value = n) %>% replace(., is.na(.), 0) %>% mutate_if(is.numeric, round, 1)
 #Apply the Cadre Harmonise rules for phasing the Livelihood Coping Strategies 
-CH_LHCStable <- LHCStable %>% mutate(indicator = "LHCS", stresscrisisemergency = StressStrategies + CrisisStrategies + EmergencyStrategies,
+CH_LhHCSCattable <- LhHCSCattable %>% mutate(indicator = "LhHCSCat", stresscrisisemergency = StressStrategies + CrisisStrategies + EmergencyStrategies,
                                      crisisemergency = CrisisStrategies + EmergencyStrategies,
                                      finalphase = case_when(
                                        EmergencyStrategies >= 20 ~ 4,
                                        crisisemergency >= 20 & EmergencyStrategies < 20 ~ 3,  
                                        NoStrategies < 80 & crisisemergency < 20 ~ 2,
                                        NoStrategies >= 80 ~ 1
-                                     )) %>% select(indicator, adm1_name, adm2_name, LHCS_NoStrategies = NoStrategies, LHCS_StressStrategies = StressStrategies, LHCS_CrisisStategies = CrisisStrategies, LHCS_EmergencyStrategies = EmergencyStrategies, LHCS_finalphase = finalphase)
+                                     )) %>% select(indicator, ADMIN1Name, ADMIN2Name, LhHCSCat_NoStrategies = NoStrategies, LhHCSCat_StressStrategies = StressStrategies, LhHCSCat_CrisisStategies = CrisisStrategies, LhHCSCat_EmergencyStrategies = EmergencyStrategies, LhHCSCat_finalphase = finalphase)
 
 
 
@@ -189,13 +189,13 @@ CH_LHCStable <- LHCStable %>% mutate(indicator = "LHCS", stresscrisisemergency =
 
 #Create a table of the proportion of people who "experienced a shock in the last six months"
 choc_subitable <- data %>% 
-  group_by(adm1_name, adm2_name) %>%  mutate(indicator = "Pendant les six dernier mois, le menage a-t-il subi un choc?") %>%
+  group_by(ADMIN1Name, ADMIN2Name) %>%  mutate(indicator = "Pendant les six dernier mois, le menage a-t-il subi un choc?") %>%
   count(choc_subi) %>%
   drop_na() %>%
   mutate(n = 100 * n / sum(n)) %>%
   ungroup() %>%
   spread(key = choc_subi, value = n) %>% replace(., is.na(.), 0) %>% mutate_if(is.numeric, round, 1) %>%
-  select(adm1_name, adm2_name, `01_choc_subi_Non` = Non, `01_choc_subi_Oui` = Oui)
+  select(ADMIN1Name, ADMIN2Name, `01_choc_subi_Non` = Non, `01_choc_subi_Oui` = Oui)
 
 
 # Merge key variables -----------------------------------------------------
@@ -203,11 +203,11 @@ choc_subitable <- data %>%
 ### Merge key variables from Direct Evidence and Contributing factor tables together 
 matrice_intermediaire <- bind_cols(
   select(CH_FCGtable,-"indicator"),# select all variables except indicator 
-  select(CH_HDDStable,-c("indicator","adm1_name","adm2_name")),# select all variables except indicator, adm1_name and adm2_name because the latter two are already selected from the table CH_FCGtable
-  select(CH_HHStable,-c("indicator","adm1_name","adm2_name")),
-  select(CH_LHCStable,-c("indicator","adm1_name","adm2_name")),
-  select(CH_rCSItable,-c("indicator","adm1_name","adm2_name")),
-  select(choc_subitable,-c("adm1_name","adm2_name"))
+  select(CH_HDDStable,-c("indicator","ADMIN1Name","ADMIN2Name")),# select all variables except indicator, ADMIN1Name and ADMIN2Name because the latter two are already selected from the table CH_FCGtable
+  select(CH_HHStable,-c("indicator","ADMIN1Name","ADMIN2Name")),
+  select(CH_LhHCSCattable,-c("indicator","ADMIN1Name","ADMIN2Name")),
+  select(CH_rCSItable,-c("indicator","ADMIN1Name","ADMIN2Name")),
+  select(choc_subitable,-c("ADMIN1Name","ADMIN2Name"))
 )
 
 
@@ -284,7 +284,7 @@ matrice_intermediaire <- cbind(matrice_intermediaire, Z1_DPME_C, Z1_DPME_P, Z1_D
 
 #re-orders the table
 matrice_intermediaire <- matrice_intermediaire %>% 
-  select(adm1_name,adm2_name,Population,Geocode,everything())
+  select(ADMIN1Name,ADMIN2Name,Population,Geocode,everything())
 
 
 # saving final data as excel sheet ----------------------------------------
